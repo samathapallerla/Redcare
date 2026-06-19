a15e68c85d51934d152c034db4f64d1a9f9ceaf3
samathapallerla <samathapallerla.9@gmail.com>
2026-06-19 14:27:03 +0200

    My first commit: Initial setup

---
+++ src/main/java/com/github/reposcore/RepoScoreApplication.java
@@ -0,0 +1,12 @@
+package com.github.reposcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoScoreApplication.class, args);
+    }
+}
